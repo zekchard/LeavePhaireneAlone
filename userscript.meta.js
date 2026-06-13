3d30997aec256620577533a217efab3851d9b919
zekchard <87356944+zekchard@users.noreply.github.com>
2026-06-13 20:34:48 +0800

    Create userscript.meta.js

---
+++ userscript.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Leave #Phairene alone!
+// @namespace    http://tampermonkey.net/
+// @version      0.2.0
+// @description  Block all users on Twitter/X who spam the Phairene tag. Based on Block BecomingElla's Twitter Blocklist (by Ella Keens).
+// @author       zekchard
+// @match        https://x.com/*
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_deleteValue
+// @downloadURL  https://raw.githubusercontent.com/zekchard/LeavePhaireneAlone/main/userscript.js
+// @updateURL  https://raw.githubusercontent.com/zekchard/LeavePhaireneAlone/main/userscript.meta.js
+// @require  	https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
+// @license MIT
+// ==/UserScript==
