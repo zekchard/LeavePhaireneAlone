c30d922fff45f7552c989c08662a03fdd6f6afe7
zekchard <87356944+zekchard@users.noreply.github.com>
2026-06-13 20:49:29 +0800

    Update and rename userscript.js to userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -8,7 +8,7 @@
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant GM_deleteValue
-// @downloadURL  https://raw.githubusercontent.com/zekchard/LeavePhaireneAlone/main/userscript.js
+// @downloadURL  https://raw.githubusercontent.com/zekchard/LeavePhaireneAlone/main/userscript.user.js
 // @updateURL  https://raw.githubusercontent.com/zekchard/LeavePhaireneAlone/main/userscript.meta.js
 // @require  	https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // @license MIT
